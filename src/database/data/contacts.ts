1b3b2ac89051563f2eda27accc7b0c06c037b393
uni <68980965+CryptoUnico@users.noreply.github.com>
2021-01-01 03:17:06 -0600

    Update contacts.ts

--- src/database/data/contacts.ts
+++ src/database/data/contacts.ts
@@ -3,7 +3,7 @@ import { ExtendedContact, TUuid } from '@types';
 export const defaultContacts: { [key in string]: ExtendedContact } = {
   'a1acf1f2-0380-5bd6-90c3-2b4a0974a6fe': {
     label: 'MyCrypto Tip Jar',
-    address: '0x4bbeEB066eD09B7AEd07bF39EEe0460DFa261520',
+    address: '0xFd63Bf84471Bc55DD9A83fdFA293CCBD27e1F4C8',
     notes: 'Toss us a coin!',
     network: 'Ethereum',
     uuid: 'a1acf1f2-0380-5bd6-90c3-2b4a0974a6fe' as TUuid
